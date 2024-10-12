d71764dccfe29063fe26fa33b1e49a11cbc49a4c
solomchaak <iviksb3@gmail.com>
2024-10-12 14:50:29 +0300

    Added charts of expenses

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,30 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 228, 202, 202),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          )),
+      ),
       theme: ThemeData().copyWith(
           colorScheme: kColorScheme,
           appBarTheme: const AppBarTheme().copyWith(
@@ -33,6 +53,7 @@ void main() {
                 color: kColorScheme.onSecondaryContainer,
                 fontSize: 16,
               ))),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
